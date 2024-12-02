0f0e656c36cfb209c4f89bd043967076d71f9039
clausol08 <101585391+clausol08@users.noreply.github.com>
2024-12-02 16:01:23 -0300

    se agrego clase javscript

---
+++ script.js
@@ -0,0 +1,24 @@
+let nombre = `Claudia`;
+const edad = 36; /*no cambia el valor*/
+console.log (`Hola mi nombres es ${nombre}`);
+console.log(`Tengo ${edad} años`);
+
+let resultado= 10+5;
+console.log (`El resultado es ${resultado}`);
+
+let esMayor = 10 > 5;
+console.log(`El numero 10 es mayor que 5?: ${esMayor}`);
+
+let esMenor = 5 < 2;
+console.log(`El numero 5 es menor que 2?: ${esMenor}`);
+
+let esVerdadero = true && false;
+let edad2 =21;
+if (edad >= 18) {
+    console.log(`Eres mayor de edad`);
+}else{
+    console.log(`Eres menor de edad`)
+}
+
+let edad =15;
+let edad_2=20;
